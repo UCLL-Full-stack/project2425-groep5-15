d9d9d41eb69c34d00343e69bc1df3fb12a5f9b16
Kaj Es <kaj.es@student.ucll.be>
2024-11-03 22:26:43 +0100

    service test

---
+++ back-end/test/service/show.service.test.ts
@@ -0,0 +1,37 @@
+import { after } from "node:test";
+import {Show} from "../../model/show";
+import showService from "../../service/show.service";
+import showDB from "../../repository/show.db";
+import e from "express";
+
+let mockShowDBGetsAllShows: jest.SpyInstance<Show[], [],any>
+
+
+beforeEach(() => {
+    mockShowDBGetsAllShows = jest.spyOn(showDB, 'getAllShows');
+});
+afterEach(() => {
+    jest.clearAllMocks();
+});
+
+test('given a database with shows when getAllShows then return all shows',() => {
+    const getAllShows = showService.getAllShows();
+
+
+    expect(mockShowDBGetsAllShows).toHaveBeenCalledTimes(1);
+    expect(getAllShows).toEqual(mockShowDBGetsAllShows.mock.results[0].value);
+    console.log(getAllShows);
+
+
+});
+
+test('given a database with shows when getShowsByDate then return shows with that date are returned',() => {
+    const getShowsByDate = showService.getShowsByDate(new Date('2024-12-01'));
+
+
+    expect(mockShowDBGetsAllShows).toHaveBeenCalledTimes(1);
+    expect(getShowsByDate).toHaveLength(2);
+
+
+});
+
