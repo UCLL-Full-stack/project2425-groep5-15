a362d355c8e4f56216374d44ef5b505a3409bd7c
Matheo Ploegaerts <matheo.ploegaerts@student.ucll.be>
2024-12-12 13:27:13 +0100

    Added pages

--- front-end/pages/movies/index.tsx
+++ front-end/pages/movies/index.tsx
@@ -31,7 +31,7 @@ const Home: React.FC = () => {
         <h1>Discover our movies!</h1>
         <p>Click on a movie to see more details</p>
         <p>Or select a date to see the planned shows</p>
-        <p>(For testing: Er staan voorlopig enkel shows gepland op 1 en 2 dec 2024)</p>
+        <p>(For testing: Er staan voorlopig enkel shows gepland op 30 dec 2024)</p>
         <section>
           {movies && <MoviesOverviewTable movies={movies} />}
         </section>
