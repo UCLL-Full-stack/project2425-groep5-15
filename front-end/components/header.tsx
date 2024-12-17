edab798a4c245d3772ae9e3fdd75bc39839c0154
Matheo Ploegaerts <matheo.ploegaerts@student.ucll.be>
2024-12-17 11:51:33 +0100

    Langage almost working

--- front-end/components/header.tsx
+++ front-end/components/header.tsx
@@ -1,14 +1,18 @@
 import Link from 'next/link';
 import { useEffect, useState } from "react";
+import { useTranslation } from 'next-i18next';
+import Language from './language/Language';
+import { User } from '@types';
 
 const Header: React.FC = () => {
-  const [loggedInUser, setLoggedInUser] = useState<string | null>(null);
+  const { t } = useTranslation('common');
+  const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
   
   useEffect(() => {
     const user = localStorage.getItem("loggedInUser");
     if (user) {
-      const parsedUser = JSON.parse(user);
-      setLoggedInUser(parsedUser.fullname.split(' ')[0]); // Assuming fullname is in the format "Firstname Lastname"
+      const parsedUser: User = JSON.parse(user);
+      setLoggedInUser(parsedUser);
     }
   }, []);
 
@@ -21,42 +25,44 @@ const Header: React.FC = () => {
     <header className="p-3 mb-3 border-bottom bg-dark bg-gradient">
       <a className="fs-2 d-flex justify-content-center mb-2 mb-lg-0 text-white-50 text-decoration-none">
         {' '}
-        Cinematic
+        {t('app.title')}
       </a>
-      <nav className="nav justify-content-center">
+      <nav className="nav justify-content-center align-items-center">
         <Link href="/" className="nav-link px-4 fs-5 text-white">
-          Home
+          {t('header.home')}
         </Link>
         <Link href="/movies" className="nav-link px-4 fs-5 text-white">
-          Movies
+          {t('header.movies')}
         </Link>
         <Link href="/shows" className="nav-link px-4 fs-5 text-white">
-          Shows
+          {t('header.shows')}
         </Link>
         {!loggedInUser && (
           <Link
             href="/login" className="nav-link px-4 fs-5 text-white"
           >
-            Login
+            {t('header.login')}
           </Link>
         )}
         {loggedInUser && (
           <>
+            <span className="nav-link px-4 fs-5 text-white">
+              {t('header.welcome')}{loggedInUser.firstName}
+            </span>
             <a
               href="/login"
               onClick={handleClick} className="nav-link px-4 fs-5 text-white"
             >
-              Logout
+              {t('header.logout')}
             </a>
-            <span className="nav-link px-4 fs-5 text-white">
-              Welcome, {loggedInUser}
-            </span>
-
           </>
         )}
+        <Language />
       </nav>
     </header>
   );
 };
+
+
 
 export default Header;
